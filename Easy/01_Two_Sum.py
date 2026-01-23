f64d918a5df5ecdc5180ddf0be924dab811a2129
Sagar D <sagardevaraj07@gmail.com>
2026-01-23 18:08:13 +0530

    Update 01_Two_Sum.py

--- Easy/01_Two_Sum.py
+++ Easy/01_Two_Sum.py
@@ -9,4 +9,12 @@ class Solution:
                 return [num[diff],i]
             num[nums[i]] = i
         return []
-        
+    
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num = {}
+        for i,val in enumerate(nums):
+            diff = target - val
+            if diff in num:
+                return [i,num[diff]]
+            num[val] = i
