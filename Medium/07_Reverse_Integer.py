72d36525eb3386ccb9c4526df724825226d79570
Sagar D <sagardevaraj07@gmail.com>
2026-01-24 21:56:40 +0530

    Update 07_Reverse_Integer.py

--- Medium/07_Reverse_Integer.py
+++ Medium/07_Reverse_Integer.py
@@ -7,3 +7,15 @@ class Solution:
         if res > 2 ** 31 -1 or res < -2 ** 31:
             return 0
         return res
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = 1 if x > 0 else -1
+        x = abs(x)
+        rev = 0
+        while x > 0:
+            rev = (rev * 10) + (x % 10)
+            x //= 10
+        if rev > 2 ** 31 -1:
+            return 0
+        return rev * sign
